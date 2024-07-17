3c09158f6d138f7033e820b5524e335a986a2705
tjakkoju93 <jakkoju1993@gmail.com>
2024-07-17 15:13:34 +0530

    first commit

--- Backend/model/employeeModel.js
+++ Backend/model/employeeModel.js
@@ -1,33 +1,56 @@
-const mongoose = require ('mongoose')
+const mongoose = require("mongoose");
 
-const employeeScchema =  new mongoose.Schema({
-    fullName :{
-        type:String,
-        required:true,
-        trim:true,
-        maxlength: [50, 'Name cannot be more than 50 characters']
-    },
-    email:{
-        type:String,
-        required:true,
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    employeerId:{
-        type:Number,
-        required:true
-    },
-    doj:{
-        type:Date,
-        required:true
-    },
-    role:{
-        type:Date,
-        enum:[employee,employeer],
-        required:true
-    },
-    
+const employeeScchema = new mongoose.Schema({
+  nameOfCompany: {
+    Type: string,
+    maxlength: [50, "Name cannot be more than 50 characters"],
+  },
+  companyid: {
+    Type: number,
+    Required: true,
+  },
+  firstName: {
+    Type: string,
+    Required: true,
+    maxlength: [30, "Name cannot be more than 30 characters"],
+  },
+  lastName: {
+    Type: string,
+    Required: true,
+    maxlength: [30, "Name cannot be more than 30 characters"],
+  },
+  email: {
+    Type: string,
+    Required: true,
+  },
+  password: {
+    Type: string,
+    Required: true,
+  },
+  role: {
+    Type: string,
+    Enum: [Employee, Employer],
+    Required: true,
+  },
+  Technologies: {
+    Type: string,
+  },
 
-})
+  Experience: {
+    Type: string,
+  },
+  Location: {
+    Type: string,
+    Enum: [Hydedrabad, Pune, Banglore, Mumbai, Delhi],
+  },
+  GraduateYear: {
+    Type: Year,
+  },
+  Language: {
+    Type: string,
+    Enum: [Telugu, hHindi, English],
+  },
+  NoticePeriod: {
+    Type: Number,
+  },
+});
